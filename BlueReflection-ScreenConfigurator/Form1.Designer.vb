0d436d04bd2a1a453ee52f34c957291ab6c422a8
Yuvi App <info@yuvi.app>
2023-11-10 14:51:40 -0500

    Blue Reflection 1 Support working

---
+++ BlueReflection-ScreenConfigurator/Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnLoadEXE = New System.Windows.Forms.Button()
+        Me.txtLoadedEXE = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.chkbxFullscreen = New System.Windows.Forms.CheckBox()
+        Me.cbxAspectRatio = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtScreenHeight = New System.Windows.Forms.TextBox()
+        Me.txtScreenWidth = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cbxPreset = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.chkbxDepthofField = New System.Windows.Forms.CheckBox()
+        Me.btnSaveChanges = New System.Windows.Forms.Button()
+        Me.ofdBREXE = New System.Windows.Forms.OpenFileDialog()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnLoadEXE
+        '
+        Me.btnLoadEXE.Location = New System.Drawing.Point(12, 12)
+        Me.btnLoadEXE.Name = "btnLoadEXE"
+        Me.btnLoadEXE.Size = New System.Drawing.Size(75, 24)
+        Me.btnLoadEXE.TabIndex = 0
+        Me.btnLoadEXE.Text = "Load EXE"
+        Me.btnLoadEXE.UseVisualStyleBackColor = True
+        '
+        'txtLoadedEXE
+        '
+        Me.txtLoadedEXE.Location = New System.Drawing.Point(93, 13)
+        Me.txtLoadedEXE.Name = "txtLoadedEXE"
+        Me.txtLoadedEXE.ReadOnly = True
+        Me.txtLoadedEXE.Size = New System.Drawing.Size(377, 23)
+        Me.txtLoadedEXE.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.chkbxDepthofField)
+        Me.GroupBox1.Controls.Add(Me.chkbxFullscreen)
+        Me.GroupBox1.Controls.Add(Me.cbxAspectRatio)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtScreenHeight)
+        Me.GroupBox1.Controls.Add(Me.txtScreenWidth)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.cbxPreset)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 42)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(458, 165)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Resolution and Aspect Ratio"
+        '
+        'chkbxFullscreen
+        '
+        Me.chkbxFullscreen.AutoSize = True
+        Me.chkbxFullscreen.Location = New System.Drawing.Point(239, 120)
+        Me.chkbxFullscreen.Name = "chkbxFullscreen"
+        Me.chkbxFullscreen.Size = New System.Drawing.Size(79, 19)
+        Me.chkbxFullscreen.TabIndex = 8
+        Me.chkbxFullscreen.Text = "Fullscreen"
+        Me.chkbxFullscreen.UseVisualStyleBackColor = True
+        '
+        'cbxAspectRatio
+        '
+        Me.cbxAspectRatio.FormattingEnabled = True
+        Me.cbxAspectRatio.Items.AddRange(New Object() {"5:4", "4:3", "3:2", "16:9", "16:10", "21:9", "32:9"})
+        Me.cbxAspectRatio.Location = New System.Drawing.Point(92, 118)
+        Me.cbxAspectRatio.Name = "cbxAspectRatio"
+        Me.cbxAspectRatio.Size = New System.Drawing.Size(100, 23)
+        Me.cbxAspectRatio.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 121)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(76, 15)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Aspect Ratio:"
+        '
+        'txtScreenHeight
+        '
+        Me.txtScreenHeight.Location = New System.Drawing.Point(329, 80)
+        Me.txtScreenHeight.Name = "txtScreenHeight"
+        Me.txtScreenHeight.Size = New System.Drawing.Size(100, 23)
+        Me.txtScreenHeight.TabIndex = 4
+        '
+        'txtScreenWidth
+        '
+        Me.txtScreenWidth.Location = New System.Drawing.Point(92, 80)
+        Me.txtScreenWidth.Name = "txtScreenWidth"
+        Me.txtScreenWidth.Size = New System.Drawing.Size(100, 23)
+        Me.txtScreenWidth.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(239, 83)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(84, 15)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Screen Height:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Screen Width:"
+        '
+        'cbxPreset
+        '
+        Me.cbxPreset.FormattingEnabled = True
+        Me.cbxPreset.Items.AddRange(New Object() {"1280x720 16:9", "1366x768 16:9", "1600x900 16:9", "1920x1080 16:9", "2560x1440 16:9", "3840x2160 16:9", "2560x1080 21:9", "3440x1440 21:9", "3840x1600 21:9", "5120x2160 21:9", "3840x1080 32:9", "5120x1440 32:9", "7680x2160 32:9"})
+        Me.cbxPreset.Location = New System.Drawing.Point(150, 46)
+        Me.cbxPreset.Name = "cbxPreset"
+        Me.cbxPreset.Size = New System.Drawing.Size(156, 23)
+        Me.cbxPreset.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(102, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Preset:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(409, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select a preset resolution below or set one via the screen width/height fields."
+        '
+        'chkbxDepthofField
+        '
+        Me.chkbxDepthofField.AutoSize = True
+        Me.chkbxDepthofField.Location = New System.Drawing.Point(329, 120)
+        Me.chkbxDepthofField.Name = "chkbxDepthofField"
+        Me.chkbxDepthofField.Size = New System.Drawing.Size(100, 19)
+        Me.chkbxDepthofField.TabIndex = 8
+        Me.chkbxDepthofField.Text = "Depth of Field"
+        Me.chkbxDepthofField.UseVisualStyleBackColor = True
+        '
+        'btnSaveChanges
+        '
+        Me.btnSaveChanges.Location = New System.Drawing.Point(353, 213)
+        Me.btnSaveChanges.Name = "btnSaveChanges"
+        Me.btnSaveChanges.Size = New System.Drawing.Size(117, 41)
+        Me.btnSaveChanges.TabIndex = 3
+        Me.btnSaveChanges.Text = "Save Changes"
+        Me.btnSaveChanges.UseVisualStyleBackColor = True
+        '
+        'ofdBREXE
+        '
+        Me.ofdBREXE.FileName = "OpenFileDialog1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 242)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(63, 15)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "V1.0 - Yuvi"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 266)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnSaveChanges)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtLoadedEXE)
+        Me.Controls.Add(Me.btnLoadEXE)
+        Me.Name = "Form1"
+        Me.Text = "Blue Reflection Screen Configurator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLoadEXE As Button
+    Friend WithEvents txtLoadedEXE As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents chkbxFullscreen As CheckBox
+    Friend WithEvents cbxAspectRatio As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtScreenHeight As TextBox
+    Friend WithEvents txtScreenWidth As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cbxPreset As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents chkbxDepthofField As CheckBox
+    Friend WithEvents btnSaveChanges As Button
+    Friend WithEvents ofdBREXE As OpenFileDialog
+    Friend WithEvents Label6 As Label
+End Class
---
+++ BlueReflection-ScreenConfigurator/Form1.vb
@@ -0,0 +1,291 @@
+﻿Imports System.IO
+
+Public Class Form1
+    'Global Stuff
+    Dim BR_Game As String = ""
+    Dim ARHexValue As UInt32 = 0
+
+    Private Sub btnLoadEXE_Click(sender As Object, e As EventArgs) Handles btnLoadEXE.Click
+        Dim BREXE = ""
+        If ofdBREXE.ShowDialog = DialogResult.OK Then
+            BREXE = ofdBREXE.FileName
+            txtLoadedEXE.Text = BREXE
+        End If
+
+        'Check If Unpacked
+        If CheckifSteamless() <> 0 Then
+            MessageBox.Show("EXE is not unpacked.. Please use STEAMLESS DRM Remover to unpack then try again.")
+            txtLoadedEXE.Clear()
+            Exit Sub
+        End If
+
+        'Get Game
+        If Path.GetFileNameWithoutExtension(BREXE).ToLower.Contains("second light") Then
+            BR_Game = "BR2"
+        Else
+            BR_Game = "BR"
+        End If
+
+        'Get INI Config
+        Get_INI_Config()
+    End Sub
+
+    'Get/Check Functions
+    Public Function CheckifSteamless()
+        Using br As BinaryReader = New BinaryReader(File.Open(txtLoadedEXE.Text, FileMode.Open))
+            br.BaseStream.Seek(&H40, SeekOrigin.Begin)
+            Dim checkbytes = br.ReadUInt32
+            Return checkbytes
+        End Using
+    End Function
+    Public Function Get_INI_Config()
+        Dim BRINILocation = Environment.GetEnvironmentVariable("USERPROFILE") & "\Documents\KoeiTecmo\BLUE REFLECTION\BRsetting.ini"
+        Dim BR2INILocation = Environment.GetEnvironmentVariable("USERPROFILE") & "\Documents\KoeiTecmo\BLUE REFLECTION Second Light\Setting.ini"
+
+        'Check If Onedrive Folder
+        If File.Exists(BRINILocation) = False Then
+            If File.Exists(BR2INILocation) = False Then
+                BRINILocation = Environment.GetEnvironmentVariable("OneDriveConsumer") & "\Documents\KoeiTecmo\BLUE REFLECTION\BRsetting.ini"
+                BR2INILocation = Environment.GetEnvironmentVariable("OneDriveConsumer") & "\Documents\KoeiTecmo\BLUE REFLECTION Second Light\Setting.ini"
+            End If
+        End If
+
+        Select Case BR_Game
+            Case "BR"
+                If File.Exists(BRINILocation) = False Then
+                    MessageBox.Show("Cannot locate Blue Reflection Settings INI (BRsettings.ini)")
+                    Exit Function
+                End If
+
+                Dim BRFilesLines = File.ReadAllLines(BRINILocation)
+                Dim ScreenWidth = BRFilesLines(1).Split("=")(1)
+                Dim ScreenHeight = BRFilesLines(2).Split("=")(1)
+                Dim AntiQuality = BRFilesLines(3).Split("=")(1)
+                Dim DepthofField = BRFilesLines(4).Split("=")(1)
+                Dim FullScreen = BRFilesLines(6).Split("=")(1)
+
+                txtScreenHeight.Text = ScreenHeight
+                txtScreenWidth.Text = ScreenWidth
+                If FullScreen = 0 Then
+                    chkbxFullscreen.Checked = False
+                Else
+                    chkbxFullscreen.Checked = True
+                End If
+
+                If DepthofField = 0 Then
+                    chkbxDepthofField.Checked = False
+                Else
+                    chkbxDepthofField.Checked = True
+                End If
+            Case "BR2"
+                If File.Exists(BR2INILocation) = False Then
+                    MessageBox.Show("Cannot locate Blue Reflection 2 Settings INI (BRsettings.ini)")
+                    Exit Function
+                End If
+        End Select
+    End Function
+    Private Sub cbxPreset_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxPreset.SelectedIndexChanged
+        txtScreenWidth.Clear()
+        txtScreenHeight.Clear()
+        cbxPreset.Text = ""
+        Select Case cbxPreset.SelectedIndex
+            Case 0
+                txtScreenWidth.Text = "1280"
+                txtScreenHeight.Text = "720"
+                cbxAspectRatio.SelectedIndex = 3
+                ARHexValue = 1071877689
+            Case 1
+                txtScreenWidth.Text = "1366"
+                txtScreenHeight.Text = "768"
+                cbxAspectRatio.SelectedIndex = 3
+                ARHexValue = 1071877689
+            Case 2
+                txtScreenWidth.Text = "1600"
+                txtScreenHeight.Text = "900"
+                cbxAspectRatio.SelectedIndex = 3
+                ARHexValue = 1071877689
+            Case 3
+                txtScreenWidth.Text = "1920"
+                txtScreenHeight.Text = "1080"
+                cbxAspectRatio.SelectedIndex = 3
+                ARHexValue = 1071877689
+            Case 4
+                txtScreenWidth.Text = "2560"
+                txtScreenHeight.Text = "1440"
+                cbxAspectRatio.SelectedIndex = 3
+                ARHexValue = 1071877689
+            Case 5
+                txtScreenWidth.Text = "3840"
+                txtScreenHeight.Text = "2160"
+                cbxAspectRatio.SelectedIndex = 3
+                ARHexValue = 1071877689
+            Case 6
+                txtScreenWidth.Text = "2560"
+                txtScreenHeight.Text = "1080"
+                cbxAspectRatio.SelectedIndex = 5
+                ARHexValue = 1075295270
+            Case 7
+                txtScreenWidth.Text = "3440"
+                txtScreenHeight.Text = "1440"
+                cbxAspectRatio.SelectedIndex = 5
+                ARHexValue = 1075372942
+            Case 8
+                txtScreenWidth.Text = "3840"
+                txtScreenHeight.Text = "1600"
+                cbxAspectRatio.SelectedIndex = 5
+                ARHexValue = 1075419546
+            Case 9
+                txtScreenWidth.Text = "5120"
+                txtScreenHeight.Text = "2160"
+                cbxAspectRatio.SelectedIndex = 5
+                ARHexValue = 1075295270
+            Case 10
+                txtScreenWidth.Text = "3840"
+                txtScreenHeight.Text = "1080"
+                cbxAspectRatio.SelectedIndex = 6
+                ARHexValue = 1080266297
+            Case 11
+                txtScreenWidth.Text = "5120"
+                txtScreenHeight.Text = "1440"
+                cbxAspectRatio.SelectedIndex = 6
+                ARHexValue = 1080266297
+            Case 12
+                txtScreenWidth.Text = "7680"
+                txtScreenHeight.Text = "2160"
+                cbxAspectRatio.SelectedIndex = 6
+                ARHexValue = 1080266297
+
+        End Select
+    End Sub
+    Private Sub cbxAspectRatio_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxAspectRatio.SelectedIndexChanged
+        Select Case cbxAspectRatio.SelectedIndex
+            Case 0
+                ARHexValue = 1067450368
+            Case 1
+                ARHexValue = 1068149419
+            Case 2
+                ARHexValue = 1069547520
+            Case 3
+                ARHexValue = 1071877689
+            Case 4
+                ARHexValue = 1070386381
+            Case 5
+                ARHexValue = 1075372942
+            Case 6
+                ARHexValue = 1080266297
+        End Select
+    End Sub
+
+    'Write Functions
+    Public Function WriteNewAspectRatio()
+        Dim StartofARHexPOS
+        Dim FoundARPOS = False
+        Dim ARIntList As New List(Of Integer)
+        ARIntList.Add(1067450368)
+        ARIntList.Add(1068149419)
+        ARIntList.Add(1069547520)
+        ARIntList.Add(1071877689)
+        ARIntList.Add(1070386381)
+        ARIntList.Add(1075372942)
+        ARIntList.Add(1080266297)
+
+        If BR_Game = "BR" Then
+            'Find the Start of Aspect Ratio
+            Using br As BinaryReader = New BinaryReader(File.Open(txtLoadedEXE.Text, FileMode.Open))
+                'First lets see if we know this spot already
+                StartofARHexPOS = &H13CF010
+                br.BaseStream.Seek(&H13CF010, SeekOrigin.Begin)
+                br.BaseStream.Seek(16, SeekOrigin.Current)
+                Dim checkbytes3 = br.ReadUInt32
+                If checkbytes3 = 1380010051 Then
+                    FoundARPOS = True
+                End If
+
+                'If we cant find the easy way we will try it manually.
+                If FoundARPOS = False Then
+                    While True
+                        Dim CheckBytes = br.ReadUInt32
+                        For Each AR In ARIntList
+                            If CheckBytes = AR Then
+                                Dim lastpos = br.BaseStream.Position
+                                br.BaseStream.Seek(12, SeekOrigin.Current)
+                                Dim checkbytes2 = br.ReadUInt32
+                                If checkbytes2 = 1380010051 Then
+                                    StartofARHexPOS = br.BaseStream.Position - 16
+                                    Exit While
+                                Else
+                                    br.BaseStream.Seek(lastpos, SeekOrigin.Begin)
+                                End If
+                            End If
+                        Next
+                    End While
+                End If
+            End Using
+
+            Using bw As BinaryWriter = New BinaryWriter(File.Open(txtLoadedEXE.Text, FileMode.Open))
+                bw.BaseStream.Seek(StartofARHexPOS, SeekOrigin.Begin)
+                bw.Write(ARHexValue)
+            End Using
+        ElseIf BR_Game = "BR2" Then
+
+        End If
+
+    End Function
+    Public Function WriteNewResolution()
+        Dim BRINILocation = Environment.GetEnvironmentVariable("USERPROFILE") & "\Documents\KoeiTecmo\BLUE REFLECTION\BRsetting.ini"
+        Dim BR2INILocation = Environment.GetEnvironmentVariable("USERPROFILE") & "\Documents\KoeiTecmo\BLUE REFLECTION Second Light\Setting.ini"
+
+        'Check If Onedrive Folder
+        If File.Exists(BRINILocation) = False Then
+            If File.Exists(BR2INILocation) = False Then
+                BRINILocation = Environment.GetEnvironmentVariable("OneDriveConsumer") & "\Documents\KoeiTecmo\BLUE REFLECTION\BRsetting.ini"
+                BR2INILocation = Environment.GetEnvironmentVariable("OneDriveConsumer") & "\Documents\KoeiTecmo\BLUE REFLECTION Second Light\Setting.ini"
+            End If
+        End If
+
+        Select Case BR_Game
+            Case "BR"
+                If File.Exists(BRINILocation) = False Then
+                    MessageBox.Show("Cannot locate Blue Reflection Settings INI (BRsettings.ini)")
+                    Exit Function
+                End If
+
+                Dim BRFilesLines = File.ReadAllLines(BRINILocation)
+                BRFilesLines(1) = "ScreenWidth=" & txtScreenWidth.Text
+                BRFilesLines(2) = "ScreenHeight=" & txtScreenHeight.Text
+                If chkbxFullscreen.Checked = True Then
+                    BRFilesLines(6) = "FullScreen=1"
+                Else
+                    BRFilesLines(6) = "FullScreen=0"
+                End If
+                If chkbxDepthofField.Checked = True Then
+                    BRFilesLines(4) = "DepthOfField=1"
+                Else
+                    BRFilesLines(4) = "DepthOfField=0"
+                End If
+
+                File.WriteAllLines(BRINILocation, BRFilesLines)
+
+            Case "BR2"
+                If File.Exists(BR2INILocation) = False Then
+                    MessageBox.Show("Cannot locate Blue Reflection 2 Settings INI (BRsettings.ini)")
+                    Exit Function
+                End If
+        End Select
+
+    End Function
+
+    Private Sub btnSaveChanges_Click(sender As Object, e As EventArgs) Handles btnSaveChanges.Click
+        If txtLoadedEXE.Text = "" Or txtScreenWidth.Text = "" Or txtScreenHeight.Text = "" Or cbxAspectRatio.Text = "" Then
+            Exit Sub
+        End If
+
+        'Write New Aspect Ratio First
+        WriteNewAspectRatio()
+
+        'Write New ScreenWidth/Height
+        WriteNewResolution()
+
+        MessageBox.Show("Saved Changes!")
+    End Sub
+End Class
